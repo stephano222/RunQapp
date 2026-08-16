1b51bb7afd644a225a29285a1bfe35303dbf26ad
stephano222 <2elnino2@gmail.com>
2026-08-17 07:29:05 +0900

    01

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,6 +24,11 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
 
+  # ダイジェスト付きアセットは内容が変われば名前も変わるので、長期キャッシュさせる
+  config.public_file_server.headers = {
+    "Cache-Control" => "public, max-age=#{1.year.to_i}, immutable"
+  }
+
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
 
